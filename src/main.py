efab3fcaa4323a2c3336cfc12926daee63c1fe05
m2waters <mwaters2110@gmail.com>
2024-12-13 21:10:29 +0000

    added summary statistics and input of whether to display them

--- src/main.py
+++ src/main.py
@@ -1,5 +1,41 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('data\\all_data.csv')
+def load_data(path):
+    
+    df = pd.read_csv(path)
+    
+    return df
 
-print(df.head())
+
+def summary_statistics(df):
+
+    print(df.head())
+    print(df.describe())
+
+    df_corr = df.drop(["Country"], axis=1)
+    correlation_matrix = df_corr.corr()
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+    plt.show()
+    plt.clf()
+
+
+if __name__ == "__main__":
+
+    df = load_data('data\\all_data.csv')
+
+    print("Would you like to see summary statitics? (Y/N)")
+    
+    x = 0
+    while x == 0:
+        summary = input()
+        if summary == "Y" or summary == "":
+            x = 1
+            print("Summary Statistics:")
+            summary_statistics(df)
+        elif summary == "N":
+            x = 1
+            print("Summary statistics will not be displayed")
+        else:
+            print("Please input Y or N to make a decision:")
