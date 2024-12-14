da44d10f5ff4bb1ba29d40c30bcb2649c00a393c
m2waters <mwaters2110@gmail.com>
2024-12-14 21:55:57 +0000

    class updated to take color argument for graph

--- src/country.py
+++ src/country.py
@@ -7,11 +7,11 @@ class country:
         self.name = name
         self.dataframe = dataframe
 
-    def plot_life_expectancy(self, ax=plt, title: bool=True, mean=None):
-        ax.plot(self.dataframe["year"], self.dataframe["life_expectancy"], label=self.name)
+    def plot_life_expectancy(self, ax=plt, title: bool=True, mean=None, color="blue"):
+        ax.plot(self.dataframe["year"], self.dataframe["life_expectancy"], label=self.name, color=color)
 
         if mean != None:
-            ax.plot(self.dataframe["year"], mean)
+            ax.plot(self.dataframe["year"], mean, color="tab:gray")
 
         if title:
             ax.set_title(f"{self.name} Life Expectancy vs Year")
