d61f991455263d43a5a612a04bed4704e0a9f4b6
m2waters <mwaters2110@gmail.com>
2024-12-14 16:43:45 +0000

    added function to calculate mean life expectancy per year and then plot the data

--- src/main.py
+++ src/main.py
@@ -58,6 +58,14 @@ def group_by_country(df):
     return (chile, china, germany, mexico, united_states, zimbabwe)
     #for country in countries:
 
+def calculate_global_mean(df):
+
+    life_expactancy_mean = df.groupby('year')['life_expectancy'].mean()
+    
+    mean_df = life_expactancy_mean.aggregate(lambda x: tuple(round(x, 2)))
+    return mean_df
+    
+
 
 
 
@@ -95,12 +103,26 @@ if __name__ == "__main__":
 
     chile, china, germany, mexico, united_states, zimbabwe = group_by_country(df)
 
-    list_of_countries = [chile, china, germany, mexico, united_states, zimbabwe]
-    plt.figure(figsize=(15, 10))
+    list_of_countries = group_by_country(df)
+    mean_life_expectancy = calculate_global_mean(df)
+
+    plt.figure(figsize=(12, 8))
     for i in list_of_countries:
         i.plot_life_expectancy(title=False)
-    
+    plt.plot(chile.dataframe['year'].unique(), mean_life_expectancy, label="mean", linestyle='--')
     plt.legend(loc="center right", bbox_to_anchor=(1.10, 0.63), ncol=1, fancybox=True, shadow=True)
     plt.suptitle("Plot of the life expectancy at birth against Year")
+
+    
+    fig, axs = plt.subplots(2, 3, figsize=(18, 8))
+    
+    chile.plot_life_expectancy(ax=axs[0,0], mean=mean_life_expectancy)
+    china.plot_life_expectancy(ax=axs[0, 1], mean=mean_life_expectancy)
+    germany.plot_life_expectancy(axs[0, 2])
+    mexico.plot_life_expectancy(axs[1, 0])
+    united_states.plot_life_expectancy(axs[1, 1])
+    zimbabwe.plot_life_expectancy(axs[1, 2])
+    
+
     # zimbabwe.plot_life_expectancy(title=False)
     plt.show()
