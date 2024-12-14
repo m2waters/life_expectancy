70a437a78fe60a163c0a3f1ba71a5d240c502f8b
m2waters <mwaters2110@gmail.com>
2024-12-14 21:08:15 +0000

    tidied up main.py and added comments

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import re
 from country import country
 
+
 def load_data(path):
-    
+    # Loads the data csv into a dataframe
     df = pd.read_csv(path)
     df.rename(columns={"Country": "country", "Year": "year", "Life expectancy at birth (years)" : "life_expectancy", "GDP": "gdp"}, inplace=True)
     
@@ -13,6 +14,8 @@ def load_data(path):
 
 
 def summary_statistics(df):
+    # provides df summary statistics and a correlation matrix heatmap
+    # User can choose to see summary statistics or not
 
     print(df.head())
     print(df.describe())
@@ -20,26 +23,33 @@ def summary_statistics(df):
 
     df_corr = df.drop(["country"], axis=1)
     correlation_matrix = df_corr.corr()
+
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
     plt.show()
     plt.clf()
 
+
 def sluggify(country):
+    # turns country names into slugs
 
     country = country.lower()
     special_characters = r'[^A-Za-z0-9\s-]'
     country = re.sub(special_characters, "", country)
     country = re.sub(r'\s+', "_", country)
 
-
     return country
 
+
 def create_country_instance(country_string, dataframe):
+    # initialises an instance of the country class
+
     country_string_cleaned = sluggify(country_string)
+
     return country(country_string_cleaned, dataframe)
 
 
 def group_by_country(df):
+    # creates dataframes for each country
 
     countries = df["country"].unique()
 
@@ -56,29 +66,17 @@ def group_by_country(df):
     print(zimbabwe.dataframe)
 
     return (chile, china, germany, mexico, united_states, zimbabwe)
-    #for country in countries:
+    
 
 def calculate_global_mean(df):
+    # calculates the mean life expectancy by grouping by year and then
+    # aggregating them into a tuple
 
     life_expactancy_mean = df.groupby('year')['life_expectancy'].mean()
     
     mean_df = life_expactancy_mean.aggregate(lambda x: tuple(round(x, 2)))
+
     return mean_df
-    
-
-
-
-
-def plot_country_vs_life_expectancy(country):
-
-    
-
-    plt.plot(country.dataframe["year"], country.dataframe["life_expectancy"])
-
-    plt.suptitle(f"Graph showing life expectancy vs year for {country.name}")
-
-    plt.show()
-    plt.clf()
 
 
 
@@ -88,6 +86,7 @@ if __name__ == "__main__":
 
     print("Would you like to see summary statitics? (Y/N)")
     
+    # while loop to determine whether summary statistics are shown
     x = 0
     while x == 0:
         summary = input()
@@ -101,19 +100,24 @@ if __name__ == "__main__":
         else:
             print("Please input Y or N to make a decision:")
 
-    chile, china, germany, mexico, united_states, zimbabwe = group_by_country(df)
 
+    chile, china, germany, mexico, united_states, zimbabwe = group_by_country(df)
     list_of_countries = group_by_country(df)
+
+
     mean_life_expectancy = calculate_global_mean(df)
 
     plt.figure(figsize=(12, 8))
+
     for i in list_of_countries:
         i.plot_life_expectancy(title=False)
+
     plt.plot(chile.dataframe['year'].unique(), mean_life_expectancy, label="mean", linestyle='--')
+
     plt.legend(loc="center right", bbox_to_anchor=(1.10, 0.63), ncol=1, fancybox=True, shadow=True)
     plt.suptitle("Plot of the life expectancy at birth against Year")
 
-    
+
     fig, axs = plt.subplots(2, 3, figsize=(18, 8))
     
     chile.plot_life_expectancy(ax=axs[0,0], mean=mean_life_expectancy)
@@ -122,7 +126,5 @@ if __name__ == "__main__":
     mexico.plot_life_expectancy(axs[1, 0])
     united_states.plot_life_expectancy(axs[1, 1])
     zimbabwe.plot_life_expectancy(axs[1, 2])
-    
-
-    # zimbabwe.plot_life_expectancy(title=False)
+  
     plt.show()
